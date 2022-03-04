8388657965d16208e7c2456b3d2de047e2e9a046
Alejandro Otero <locolajefa@gmail.com>
2022-03-04 17:29:07 -0300

    Nav

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return(
+        
+        <navbar className="navbar navbar-expand-lg navbar-dark text-light">
+
+            <div className="container-fluid">
+                <h1>Navbar</h1>
+                <ul className="navbar-nav">
+                    <li className="nav-item">Home</li>
+                    <li className="nav-item">Shop</li>
+                    <li className="nav-item">About Us</li>
+                    <li className="nav-item">Sing Up</li>
+                </ul>
+            </div>
+                
+        </navbar>
+        
+
+    )
+}
+
+export default Navbar;
