bddfe76d673a72f11a23b140a611556292c56e19
Alejandro Otero <locolajefa@gmail.com>
2022-03-19 20:48:18 -0300

    Build 7
    falta terminar la descripcion extra, arreglar el loading en itemdetail y hacer la mutiplicacion de precio para el stock en detail

--- src/Container/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Container/ItemDetailContainer/ItemDetailContainer.jsx
@@ -6,21 +6,25 @@ import { getFetch } from "../../helpers/gFetch"
 
 function ItemDetailContianer() {
     const [producto, setProducto] = useState({})
-
-    const { detalleId } = useParams() 
+    const { detalleId } = useParams()
+    const [loading, setLoading] = useState(true)
     
     useEffect(()=>{
         getFetch
         .then(prod => prod.find(item => item.id === detalleId))
         .then(prod => setProducto(prod))
-        .catch(err => console.log(err))
-
+        .catch((err) => console.log(err))
+        .finally(()=> setLoading(false))
     },)
     
     return (
-        <div>
-            <ItemDetail producto={producto} />      
-        </div>
+        <>
+                {loading ? 
+                    <h2><i className="mt-5 fa-regular fa-hourglass"></i>Loading your game's details...</h2>
+                    :
+                    <ItemDetail producto={producto} />                       
+            }
+        </>
     )
 }
 
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,42 +1,29 @@
 import ItemCount from "../ItemCount/ItemCount"
 import { Row, Col } from 'react-bootstrap'
-import { useEffect, useState } from 'react'
-import { getFetch } from '../../helpers/gFetch'
 
 function ItemDetail({producto}) {
-  const [loading, setLoading] = useState(true)
-
-    useEffect(()=>{
-        getFetch 
-        .catch((err) => console.log(err))
-        .finally(()=> setLoading(false))
-        })
 
   const {imgSource, name, desc, descExtra, price, stock} = producto;
 
   return (
 
     <div className="container mt-5">
-      {loading ? 
-            <h2><i className="fa-regular fa-hourglass"></i>Loading Your Game's Page...</h2>
-            :
-            <Row>
-              <Col sm={8}>
-                <img className="imgSource" src={imgSource} alt="imagen" />
-              </Col>
-              <Col sm={4} className="column-2 text-center">
-                <h2 className="mb-5"> {name} </h2>
-                <h5 className="mb-5"> {desc} </h5>
-                <h5 className="mb-5"> {descExtra} </h5>
-                <h3 className="mb-5"> ${price} </h3>
-                <div>
-                  <div className="flex">
-                    <ItemCount stock={stock} initial={1} />
-                  </div>
-                </div>
-              </Col>
-            </Row>                       
-            }
+      <Row>
+        <Col sm={8}>
+          <img className="imgSource" src={imgSource} alt="imagen" />
+        </Col>
+        <Col sm={4} className="column-2 text-center">
+          <h2 className="mb-5"> {name} </h2>
+          <h5 className="mb-5"> {desc} </h5>
+          <h5 className="mb-5"> {descExtra} </h5>
+          <h3 className="mb-5"> ${price} </h3>
+          <div>
+            <div className="flex">
+              <ItemCount stock={stock} initial={1} />
+            </div>
+          </div>
+        </Col>
+      </Row>                       
     </div>
 
   )
