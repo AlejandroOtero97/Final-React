e774e2a2602e4233f934a563bbd89b1543a8e7ce
Alejandro Otero <locolajefa@gmail.com>
2022-04-19 21:56:27 -0300

    Final 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import NavBar from './components/Navbar/Navbar';
 import Cart from './components/Cart/Cart';
 import ItemDetailContainer from './Container/ItemDetailContainer/ItemDetailContainer';
-import ItemDetailContainerLocal from './Container/ItemDetailContainer/ItemDetailContainerLocal';
-
 import ItemListContainer from './Container/ItemListContainer.jsx';
-import ItemListContainerLocal from './Container/ItemListContainerLocal.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css/animate.min.css';
@@ -20,16 +17,8 @@ function App() {
                         <div className="App">
                             <NavBar />
                             <Routes>
-
-                                <Route path='/details/:productId' element={<ItemDetailContainerLocal />}/>
-                                <Route path='/details/:productId' element={<ItemDetailContainer />}/>
-
-                                
-                                
-                                
-                                
                                 <Route path="/" element={<ItemListContainer greeting="Welcome to the React Shop" />} />
-                                <Route path='/category/parts' element={<ItemListContainerLocal greeting="Welcome to the React Shop" />} />
+                                <Route path='/details/:productId' element={<ItemDetailContainer />}/>     
                                 <Route path='/category/games' element={<ItemListContainer />}/>    
                                 <Route path='/Cart' element={<Cart />}/>
                                 <Route path='/*' element={<Navigate to='/' />} />
